fd27e0d57335147d1e8039cdcacbbdf3907cfd1e
banana-dog <fanochka_sitnikova@mail.ru>
2022-07-14 23:00:02 +0300

    Динозаврики разворачиваются!!

--- Assets/scripts/DinoAI.cs
+++ Assets/scripts/DinoAI.cs
@@ -7,9 +7,39 @@ public class DinoAI : AI
     void Update()
     {
         RemainingTime -= Time.deltaTime;
-        
-        var distanceVector = player.position.x - transform.position.x;
-        var direction = RemainingTime <= 0 ? Vector3.right : Vector3.Normalize(distanceVector * Vector3.right);
+        var differenceX = player.position.x - transform.position.x;
+        Vector2 direction;
+        if (RemainingTime <= 0)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector3.Normalize(differenceX * Vector2.right);
+        }
         _rb.velocity = direction * Speed;
+        if (Mathf.Abs(differenceX) > 1)
+        {
+            TurnIfNeeded();
+        }
+    }
+
+    private void TurnIfNeeded()
+    {
+        bool isGoingLeft = _rb.velocity.x < 0;
+        bool isLookingRight = transform.localScale.x > 0;
+        if (isGoingLeft && isLookingRight)
+        {
+            TurnAround();
+        }
+        if (!isGoingLeft && !isLookingRight)
+        {
+            TurnAround();
+        }
+    }
+
+    private void TurnAround()
+    {
+        transform.localScale *= new Vector2(-1, 1);
     }
 }
