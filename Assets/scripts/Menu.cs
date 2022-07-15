0ab2d09e06aa32636ba29e99cef8bc742cc95d38
banana-dog <fanochka_sitnikova@mail.ru>
2022-07-16 01:55:10 +0300

    Добавила кнопку "начать сначала"

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public GameObject MenuPanel;
+
     public void Toggle()
     {
         MenuPanel.SetActive(!MenuPanel.activeSelf);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
